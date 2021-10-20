c5cfa9ea7d52318fc271d679905412fc771eff1a
Mol0k <80773949+Mol0k@users.noreply.github.com>
2021-10-20 11:00:25 +0200

    Creando la clase contructor de la clase usuario

---
+++ controladores/usuario.js
@@ -0,0 +1,14 @@
+class Usuario {
+    constructor(id, nombre, direccion, codigoPostal, pais){
+        this.id = id
+        this.nombre = nombre
+        this.direccion = direccion
+        this.codigoPostal = codigoPostal
+        this.pais = pais
+    }
+
+    calcularLibrosPrestados() {
+
+    }
+}
+export default Usuario;
