b3c64b0698dec754ba984311c275ea110bb59829
Mol0k <80773949+Mol0k@users.noreply.github.com>
2021-10-21 13:57:47 +0200

    Update usuario.js
    Agregando los métodos crearUsuariosId, modificarUsuariosId y borrarUsuariosId

--- controladores/usuario.js
+++ controladores/usuario.js
@@ -1,5 +1,5 @@
 class Usuario {
-    constructor(id, nombre, direccion, codigoPostal, pais){
+    constructor(id, nombre, direccion, codigoPostal, pais) {
         this.id = id
         this.nombre = nombre
         this.direccion = direccion
@@ -7,8 +7,43 @@ class Usuario {
         this.pais = pais
     }
 
-    calcularLibrosPrestados() {
+    //metodo que crea los usuarios ID
+    crearUsuariosId() {
+        let letras = ['a', 'b', 'c', 'v', 'g', 'e', 'h', 'z', 'r'] //array de letras
+        let numeros = [1, 3, 5, 7, 9, 0, 6, 4, 8] //array de números
+        let usuarioID = '' //string vacio donde se va a generar la contraseña
+        for (let i = 0; i < 6; i++) { //definimos la i porque solo va a estar en el momento del bucle
+            usuarioID = usuarioID + letras[Math.ceil(Math.random() * letras.length - 1)] //generamos las letras
+            usuarioID = usuarioID + numeros[Math.ceil(Math.random() * numeros.length - 1)] //generamos los numeros
+        }
+        return usuarioID //devolvemos la contrasenha
+
+    }
+
+    //metodo que modifica los usuarios ID
+    modificarUsuarioId(modificarId) {
+        if (typeof modificarId != 'string') { //comprobamos que sea un string
+            console.log('El nuevo id tiene que ser un string.')
+        } else if (modificarId.length <= 6) { //si no tiene 6 caracteres nos devuelve este mensaje
+            console.log('El nuevo id de usuario tiene que ser de más de 6 caracteres.')
+        } else {
+            this.id = modificarId //le decimos que la contraseña  ahora va a ser nuevaContrasenha
+        }
+    }
+    //metodo que borra los usuarios ID
+    borrarUsuariosId() {
+        delete Usuario.id
+    }
+    //metodo que calculo los libros prestados y también los que están vencidos
+    libros_prestados() {
 
     }
 }
+
+let usuario = new Usuario()
+usuario.crearUsuariosId()
+
+usuario.modificarUsuarioId()
+
 export default Usuario;
+
