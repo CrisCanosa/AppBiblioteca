69b46d7107cffea96aaae7b5ce879161826fb61e
viliyou <avilasl@fpcoruna.afundacion.org>
2021-10-21 14:13:44 +0200

    Update libro.js

--- controladores/libro.js
+++ controladores/libro.js
@@ -16,13 +16,28 @@
 class Libro {
 
     constructor(id,titulo,autor,editorial,prestado,venta) {
-        this.id = id
+        this.id = id || this.getAleatoryId()
         this.titulo = titulo
         this.autor = autor 
         this.editorial = editorial
         this.prestado = this.esPrestado() // supongo que tengo que crear un método para esto
         this.venta = this.alaVenta() // supongo que aquí tengo que crear otro método para esto otro tb
     }
-}
 
+
+    getAleatoryId(){
+        let letters = ['a','b','c','d','e','f','g','h','i']
+        let numbers = [1,2,3,4,5,6,7,8,9]
+        let id = ''
+        for (let i = 0; i < 6; i++){ //la i tiene que estar definida aquí porque sólo existe en el momento que existe el bucle
+            id = id + letters[this.getAleatoryNumber(letters.length)] + numbers[this.getAleatoryNumber(numbers.length)]
+    }
+
+        return id
+    }
+
+    getAleatoryNumber(max){
+        return Math.floor(Math.random * max)
+    }
+}
 //
