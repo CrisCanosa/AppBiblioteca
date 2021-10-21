cc523268aa68b57fd1045c7c2cabd78639688edd
viliyou <avilasl@fpcoruna.afundacion.org>
2021-10-21 13:57:13 +0200

    Update libro.js

--- controladores/libro.js
+++ controladores/libro.js
@@ -1,1 +1,28 @@
-/*Vilas*/
+/*Vilas 
+
+ - LIBRO
+        - Id 
+        - Titulo
+        - Autor
+        - Editorial
+        - Fecha primera edición
+        - Está prestado
+        - Esta a la venta al publico (esto especifíca si el libro aún se vende en el mercado o ya no)
+
+
+*/
+
+
+class Libro {
+
+    constructor(id,titulo,autor,editorial,prestado,venta) {
+        this.id = id
+        this.titulo = titulo
+        this.autor = autor 
+        this.editorial = editorial
+        this.prestado = this.esPrestado() // supongo que tengo que crear un método para esto
+        this.venta = this.alaVenta() // supongo que aquí tengo que crear otro método para esto otro tb
+    }
+}
+
+//
