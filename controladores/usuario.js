a506bb36e4aa23424db41eb62dc94f2e881da220
Mol0k <80773949+Mol0k@users.noreply.github.com>
2021-11-03 10:50:05 +0100

    Update usuario.js

--- controladores/usuario.js
+++ controladores/usuario.js
@@ -19,6 +19,29 @@ class Usuario {
     //         return idAleatorio
     //     }
     // }
+
+    //método set
+   
+    setNombre(nuevoNombre){
+        this.nombre = nuevoNombre
+    }
+    
+    setPrimerApellido(nuevoApellido){
+        this.primerApellido = nuevoApellido
+    }
+    
+    setSegundoApellido(nuevo2Apellido){
+        this.segundoApellido = nuevo2Apellido
+    }
+    setfechaAlta(nuevaFA){
+        this.fechaAlta = nuevaFA
+    }
+    
+    setfechaBaja(nuevaFB){
+        this.fechaBaja = nuevaFB
+    }
+    
+    //metodo crear id de usuarios
     crearIdUsuario() {
         let letras = ['a', 'b', 'c', 'v', 'g', 'e', 'h', 'z', 'r'] //array de letras
         let numeros = [1, 3, 5, 7, 9, 0, 6, 4, 8] //array de números
@@ -27,29 +50,31 @@ class Usuario {
             usuarioID = usuarioID + letras[Math.ceil(Math.random() * letras.length - 1)] //generamos las letras
             usuarioID = usuarioID + numeros[Math.ceil(Math.random() * numeros.length - 1)] //generamos los numeros
         }
-        return usuarioID //devolvemos la contrasenha
+        return usuarioID //devolvemos el usuario ID
 
     }
 
     //metodo que modifica los usuarios 
-    modificarUsuarios(modificarNombre, modificarPrimerApellido, modificarSegundoApellido, modificarFA,modificarFB) {
+    modificarUsuarios(modificarNombre, modificarPrimerApellido, modificarSegundoApellido,modificarFA,modificarFB) {
        this.nombre = modificarNombre
        this.primerApellido = modificarPrimerApellido
        this.segundoApellido = modificarSegundoApellido
-    
+       this.fechaAlta = modificarFA
+       this.fechaBaja = modificarFB
     }    
     
 
   
     //metodo que borra los usuarios ID
     borrarUsuarios() {
-      
+        
     }
 
 
 }
-let usuario = new Usuario('Cris',null,null,null)
-console.log(usuario.id)
+let usuario = new Usuario('Cris',null,null,null,null)
+usuario.nombre = "Juan"
+
 console.log(usuario)
 
 //export default Usuario;
